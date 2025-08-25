0f1db5bb83440f7a2fae99bd365ca4c0e853e7f2
omormehn <nathanuseni@gmail.com>
2025-08-25 03:43:07 +0100

    added message body

--- app/(screens)/MessageContainer.tsx
+++ app/(screens)/MessageContainer.tsx
@@ -1,14 +1,41 @@
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { ScrollView } from 'react-native-gesture-handler'
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import { router } from 'expo-router';
+import MessageInput from '@/components/container/MessageInput';
+import Feather from 'react-native-vector-icons/Feather';
+
 
 
 const MessageContainer = () => {
-    <View style={styles.container}>
-        <ScrollView style={styles.header} stickyHeaderIndices={[0]}>
+    return (
+        <View style={styles.container}>
+
             {/* Head */}
-        </ScrollView>
-    </View>
+            <View style={styles.header}>
+                <View style={styles.headerBody} >
+                    <View className='flex-row items-center gap-6'>
+                        <AntDesign onPress={() => router.back()} name="arrowleft" size={24} color="black" />
+                        <TouchableOpacity className='flex-row items-center gap-4'>
+                            <View className='size-10 rounded-full bg-gray-300' />
+                            <Text className='font-bold text-xl'>Peter</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+            {/* Body */}
+            <ScrollView></ScrollView>
+
+            {/* Bottom */}
+            <View style={styles.inputContainer}>
+                <MessageInput />
+                <TouchableOpacity onPress={() => { }} className='bg-gray-100 py-4 px-4 rounded-full'>
+                    <Feather name='send' size={23} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 
 }
 
@@ -24,10 +51,27 @@ const styles = StyleSheet.create({
         top: 0,
         left: 0,
         right: 0,
-        height: 80,
+        height: 90,
         backgroundColor: '#f8f8f8',
-        paddingHorizontal: 30,
+        paddingHorizontal: 20,
         zIndex: 1,
-
     },
+    headerBody: {
+        marginTop: 40,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 12,
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'white',
+        padding: 15,
+
+    }
 })
