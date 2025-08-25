2a9f90e76579d49b15546533dfb3251b6fc0db13
omormehn <nathanuseni@gmail.com>
2025-08-25 02:08:19 +0100

    clean

--- app/(screens)/ChatScreen.tsx
+++ app/(screens)/ChatScreen.tsx
@@ -5,83 +5,85 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { useAuth } from '@/context/AuthContext';
 import { router } from 'expo-router';
-import SearchBar from '@/components/SearchBar';
+import Chat from '@/components/container/Chat';
 
 const ChatScreen = () => {
     const { user } = useAuth();
     const [messages, setMessages] = React.useState([]);
 
     return (
-        <ScrollView style={styles.container}>
+        <ScrollView stickyHeaderIndices={[0]} style={styles.container}>
             {/* Screen Custom Header */}
             <View style={styles.header}>
-                <View className='flex-row items-center gap-4'>
-                    <AntDesign onPress={() => router.back()} name="arrowleft" size={24} color="black" />
-                    <Text className='font-bold text-xl'>{user?.username}</Text>
+                <View className='mt-10 flex-row justify-between items-center'>
+                    <View className='flex-row  gap-4'>
+                        <AntDesign onPress={() => router.back()} name="arrowleft" size={24} color="black" />
+                        <Text className='font-bold text-xl'>{user?.username}</Text>
+                    </View>
+                    <MaterialIcons name="add-comment" size={24} color="black" />
                 </View>
-                <MaterialIcons name="add-comment" size={24} color="black" />
             </View>
             {/* Search */}
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
-            <View className='justify-center items-center  mt-10'>
-                <SearchBar width={350} />
+            <View >
+                <Chat />
             </View>
             {/* Chat Messages */}
-            <View className='flex-1'>
+            {/* <View className='flex-1'>
                 {messages.length > 0 ? (
                     messages.map((message, index) => (
                         <View key={index} className='p-4 border-b border-gray-200'>
-                            {/* <Text className='text-base'>{message.text}</Text> */}
+                            <Text className='text-base'>{message.text}</Text>
                         </View>
                     ))
                 ) : (
@@ -89,7 +91,7 @@ const ChatScreen = () => {
                         <Text className='text-gray-500'>No messages yet</Text>
                     </View>
                 )}
-            </View>
+            </View> */}
 
 
         </ScrollView>
@@ -111,8 +113,8 @@ const styles = StyleSheet.create({
         right: 0,
         height: 80,
         backgroundColor: '#f8f8f8',
-        justifyContent: 'center',
-        alignItems: 'center',
+        paddingHorizontal: 30,
         zIndex: 1, // Ensure header is on top
+
     },
 })
